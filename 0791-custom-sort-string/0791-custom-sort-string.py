ba210d98885dc42d6badb140ff93100a6d5051b8
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-03-11 22:33:00 +0530

    Time: 37 ms (46.56%), Space: 16.6 MB (26.18%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,20 +1,10 @@
-class Solution(object):
-    def customSortString(self, order, s):
-        """
-        :type order: str
-        :type s: str
-        :rtype: str
-        """
-        dic, ans, un = {}, "", []
-        for ch in s:
-            if ch not in order:
-                un.append(ch)
-            if ch not in dic: 
-                dic[ch] = 1
-            else: dic[ch] += 1
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        A = Counter(s)
+        res = []
         for ch in order:
-            if ch in dic:
-                ans += ch*dic[ch]
-        for ch in un:
-            ans += ch
-        return ans
+            res.append(A[ch]*ch)
+            A[ch] = 0
+        for ch in sorted(A.keys()):
+            res.append(ch*A[ch])
+        return ''.join(res)
