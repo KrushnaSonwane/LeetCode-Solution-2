5dbef479beeeb87b30e16e6531742b5293963ef6
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-03-13 22:41:53 +0530

    Time: 612 ms (5.93%), Space: 43.7 MB (8.89%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
@@ -0,0 +1,22 @@
+class Solution:
+    def decodeCiphertext(self, S: str, rows: int) -> str:
+        if not S: return ''
+        A, i = [], 0
+        for _ in range(rows):
+            t = []
+            for _ in range(len(S)//rows):
+                t.append(S[i])
+                i += 1
+            A.append(t)
+        res, x = [], 0
+        for _ in range(len(S)//rows):
+            i, j = 0, x
+            for _ in range(rows):
+                if j < len(S) // rows:
+                    res.append(A[i][j])
+                i += 1
+                j += 1
+            x += 1
+        while res and res[-1] == ' ':
+            res.pop()
+        return ''.join(res)
