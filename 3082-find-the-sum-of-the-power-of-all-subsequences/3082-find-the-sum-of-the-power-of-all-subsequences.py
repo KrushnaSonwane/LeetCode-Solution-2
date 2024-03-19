7bed83f15f797cf52f3878fb50c08a530bbd8a0e
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-03-19 21:55:19 +0530

    Time: 453 ms (35.76%), Space: 139.7 MB (16.62%) - LeetHub

--- 3082-find-the-sum-of-the-power-of-all-subsequences/3082-find-the-sum-of-the-power-of-all-subsequences.py
+++ 3082-find-the-sum-of-the-power-of-all-subsequences/3082-find-the-sum-of-the-power-of-all-subsequences.py
@@ -3,12 +3,8 @@ class Solution:
         n, MOD = len(A), 10**9+7
         @cache
         def dfs(i, sum_, size):
-            if sum_ == k:
-                key = n-size
-                return 2**key
-            if sum_ > k: return 0
-            if i == len(A): return 0
-            res = dfs(i+1, sum_, size) % MOD
-            res = (res + dfs(i+1, sum_+A[i], size+1)) % MOD
-            return res
-        return dfs(0, 0, 0)
+            if k == sum_: return 2 ** size
+            if sum_ > k or i == len(A): return 0
+            res = dfs(i+1, sum_, size)
+            return (res + dfs(i+1, sum_+A[i], size-1)) % MOD
+        return dfs(0, 0, n)
