4e06962a18811ad7449c038fad63b98257be7c37
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-04-19 22:34:15 +0530

    Time: 205 ms (86.50%), Space: 30.7 MB (32.41%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+        visit = set()
+        queue = []
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1' and (i, j) not in visit:
+                    visit.add((i, j))
+                    queue.append([i, j])
+                    while queue:
+                        row, col = queue.pop(0)
+                        if row >= 1 and grid[row - 1][col] == '1' and (row - 1, col) not in visit:
+                            visit.add((row - 1, col))
+                            queue.append([row - 1, col])
+                        if col >= 1 and grid[row][col - 1] == '1' and (row, col - 1) not in visit:
+                            visit.add((row, col - 1))
+                            queue.append([row, col - 1])
+                        if m > row + 1 and grid[row + 1][col] == '1' and (row + 1, col) not in visit:
+                            visit.add((row + 1, col))
+                            queue.append([row + 1, col])
+                        if n > col + 1 and grid[row][col + 1] == '1' and (row, col + 1) not in visit:
+                            visit.add((row, col + 1))
+                            queue.append([row, col + 1])
+                    res += 1
+        return res
