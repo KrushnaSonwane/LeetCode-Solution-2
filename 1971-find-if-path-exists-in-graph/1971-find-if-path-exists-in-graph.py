9d35c78ca57cda47276dc260e33ccaa8c534fda3
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-04-23 00:54:54 +0530

    Time: 1458 ms (73.93%), Space: 105.7 MB (97.46%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,22 +1,25 @@
+class UF:
+    def __init__(self, n):
+        self.rank = [0 for _ in range(n)]
+        self.p = [i for i in range(n)]
+    
+    def find(self, node):
+        if node == self.p[node]: return node
+        self.p[node] = self.find(self.p[node])
+        return self.p[node]
+    
+    def union(self, u, v):
+        u, v = self.find(u), self.find(v)
+        if u == v: return
+        if self.rank[u] > self.rank[v]:
+            self.p[v] = u
+        else:
+            self.p[u] = v
+            self.rank[v] += 1
+
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
-        neighbors = defaultdict(list)
-        for n1, n2 in edges:
-            neighbors[n1].append(n2)
-            neighbors[n2].append(n1)
-            
-        def dfs(node, end, seen):
-            if node == end:
-                return True
-            if node in seen:
-                return False
-            
-            seen.add(node)
-            for n in neighbors[node]:
-                if dfs(n, end, seen):
-                    return True
-                
-            return False
-        
-        seen = set()    
-        return dfs(start, end, seen)
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        uf = UF(n)
+        for a, b in edges:
+            uf.union(a, b)
+        return uf.find(source) == uf.find(destination)
