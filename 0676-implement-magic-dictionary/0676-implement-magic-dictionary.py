61580cc42ecb15d7260712524f8c917bdd834c40
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-04-26 18:16:50 +0530

    Time: 133 ms (77.29%), Space: 16.9 MB (67.74%) - LeetHub

--- 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
@@ -1,37 +1,19 @@
-class Trie:
-    def __init__(self):
-        self.hashT = {}
-        self.leaf = False
-
 class MagicDictionary:
 
     def __init__(self):
-        self.obj = Trie()
+        self.hashT = defaultdict(list)
 
     def buildDict(self, dictionary: List[str]) -> None:
-        for word in dictionary:
-            a = self.obj
-            for ch in word:
-                if ch not in a.hashT:
-                    a.hashT[ch] = Trie()
-                a = a.hashT[ch]
-            a.leaf = True
+        for w in dictionary:
+            self.hashT[len(w)].append(w)
 
-    def search(self, W: str) -> bool:
-        @cache
-        def dfs(i, a, change):
-            if i == len(W):
-                return change and a.leaf
-            res = False
-            if not change:
-                for ch in a.hashT:
-                    if ch == W[i]: continue
-                    res = res or dfs(i+1, a.hashT[ch], True)
-            if W[i] in a.hashT:
-                res = res or dfs(i+1, a.hashT[W[i]], change)
-            return res
-        return dfs(0, self.obj, False)
-
+    def search(self, searchWord: str) -> bool:
+        for word in self.hashT[len(searchWord)]:
+            count = 0
+            for a, b in zip(word, searchWord):
+                count += a != b
+            if count == 1: return True
+        return False
 
 
 # Your MagicDictionary object will be instantiated and called as such:
