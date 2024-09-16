3890e54bad3b874d13e8aeca1bd3a37ba18d07f5
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-09-16 18:18:55 +0530

    Time: 2960 ms (100.00%), Space: 752.8 MB (100.00%) - LeetHub

---
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxScore(self, a: List[int], b: List[int]) -> int:
+        NA = len(a)
+        NB = len(b)
+        INF = 10 ** 20
+        
+        @cache
+        def go(indexa, indexb):
+            if indexa == NA:
+                return 0
+            if indexb == NB:
+                return -INF
+            
+            best = go(indexa, indexb + 1)
+            best = max(best, go(indexa + 1, indexb + 1) + a[indexa] * b[indexb])
+            
+            return best
+        return go(0, 0) 
